f9b7ef7dfa4da4c4282d5c96476abcac330c91f5
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2012-08-08 20:23:05 +0000

    search layout changes

--- fsm/core/forms.py
+++ fsm/core/forms.py
@@ -6,11 +6,16 @@ from models import Technician, Task, TaskType
 
 class TechnicianSearchForm(forms.Form):
     technician = forms.CharField(label=_(u"Técnico"),
-                            widget=forms.TextInput(attrs={'class': 'span2'}))
-    connection_type = forms.ChoiceField(label=_(u"Tipo de Conexão"), choices=Technician.CONNECTION_TYPE_AND_EMPTY)
+                            widget=forms.TextInput(attrs={'class': 'span3'}))
+    connection_type = forms.ChoiceField(label=_(u"Tipo de Conexão"),
+                            choices=Technician.CONNECTION_TYPE_AND_EMPTY,
+                            widget=forms.Select(attrs={'class': 'span2'}))
 
 class TaskSearchForm(forms.Form):
     task = forms.CharField(label=_(u"Tarefa"),
-                            widget=forms.TextInput(attrs={'class': 'span2'}))
-    task_type = forms.ModelChoiceField(label=_(u"Tipo de Tarefa"), queryset=TaskType.objects.all())
-    status = forms.ChoiceField(label=_(u"Status"), choices=Task.STATUS_AND_EMPTY)
+                            widget=forms.TextInput(attrs={'class': 'span3'}))
+    task_type = forms.ModelChoiceField(label=_(u"Tipo de Tarefa"),
+                            queryset=TaskType.objects.all(),
+                            widget=forms.Select(attrs={'class': 'span3'}))
+    status = forms.ChoiceField(label=_(u"Status"), choices=Task.STATUS_AND_EMPTY,
+                            widget=forms.Select(attrs={'class': 'span2'}))
